7b5e3d1ff288cb4d932cb580aa6b735402ac51bf
gupteswar45 <gupteswarkumar45@gmail.com>
2026-09-02 12:17:08 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // We can always construct a uniform parity array (all odd or all even).
+        return true;
+    }
+};
